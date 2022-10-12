b8290085fb5dcb78a63e1605e9a6ec8742999deb
BamstehQ <andreyshap@wizardsdev.com>
2022-10-13 01:19:32 +0300

    add dependencies for swagger and connect swagger to project

--- src/main/java/com/example/demo/entity/Department.java
+++ src/main/java/com/example/demo/entity/Department.java
@@ -1,8 +1,12 @@
 package com.example.demo.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Department {
 
     private long dpID;
